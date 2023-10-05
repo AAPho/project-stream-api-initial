69d58b941dbcf6d871dede430e3eaa2c6b66131f
AAPho <austinanpho@gmail.com>
2023-10-04 20:57:51 -0700

    inital commit

--- src/main/java/org/example/StreamAPITutorial.java
+++ src/main/java/org/example/StreamAPITutorial.java
@@ -2,6 +2,9 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamAPITutorial {
 
@@ -21,6 +24,209 @@ public class StreamAPITutorial {
         // C2
 
         // TODO add more tutorial code here
+
+        //Different Kinds of Streams
+
+        Arrays.asList("a1", "a2", "a3")
+                .stream()
+                .findFirst()
+                .ifPresent(System.out::println);  // a1
+
+        Stream.of("a1", "a2", "a3")
+                .findFirst()
+                .ifPresent(System.out::println);  // a1
+
+        IntStream.range(1, 4)
+                .forEach(System.out::println);
+        // 1
+        // 2
+        // 3
+
+        Arrays.stream(new int[]{1, 2, 3})
+                .map(n -> 2 * n + 1)
+                .average()
+                .ifPresent(System.out::println);  // 5.0
+
+        Stream.of("a1", "a2", "a3")
+                .map(s -> s.substring(1))
+                .mapToInt(Integer::parseInt)
+                .max()
+                .ifPresent(System.out::println);  // 3
+
+        IntStream.range(1, 4)
+                .mapToObj(i -> "a" + i)
+                .forEach(System.out::println);
+
+        // a1
+        // a2
+        // a3
+
+        Stream.of(1.0, 2.0, 3.0)
+                .mapToInt(Double::intValue)
+                .mapToObj(i -> "a" + i)
+                .forEach(System.out::println);
+
+        // a1
+        // a2
+        // a3
+
+        //Processing Order
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return true;
+                });
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return true;
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+        /*
+        filter:  d2
+        forEach: d2
+        filter:  a2
+        forEach: a2
+        filter:  b1
+        forEach: b1
+        filter:  b3
+        forEach: b3
+        filter:  c
+        forEach: c
+         */
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .anyMatch(s -> {
+                    System.out.println("anyMatch: " + s);
+                    return s.startsWith("A");
+                });
+
+        // map:      d2
+        // anyMatch: D2
+        // map:      a2
+        // anyMatch: A2
+
+        //why order matters
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("A");
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+        // map:     d2
+        // filter:  D2
+        // map:     a2
+        // filter:  A2
+        // forEach: A2
+        // map:     b1
+        // filter:  B1
+        // map:     b3
+        // filter:  B3
+        // map:     c
+        // filter:  C
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("a");
+                })
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+        // filter:  d2
+        // filter:  a2
+        // map:     a2
+        // forEach: A2
+        // filter:  b1
+        // filter:  b3
+        // filter:  c
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .sorted((s1, s2) -> {
+                    System.out.printf("sort: %s; %s\n", s1, s2);
+                    return s1.compareTo(s2);
+                })
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("a");
+                })
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+        /*
+        sort:    a2; d2
+        sort:    b1; a2
+        sort:    b1; d2
+        sort:    b1; a2
+        sort:    b3; b1
+        sort:    b3; d2
+        sort:    c; b3
+        sort:    c; d2
+        filter:  a2
+        map:     a2
+        forEach: A2
+        filter:  b1
+        filter:  b3
+        filter:  c
+        filter:  d2
+        */
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("a");
+                })
+                .sorted((s1, s2) -> {
+                    System.out.printf("sort: %s; %s\n", s1, s2);
+                    return s1.compareTo(s2);
+                })
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+        // filter:  d2
+        // filter:  a2
+        // filter:  b1
+        // filter:  b3
+        // filter:  c
+        // map:     a2
+        // forEach: A2
+
+        //reusing streams
+
+        /*
+        Stream<String> stream =
+                Stream.of("d2", "a2", "b1", "b3", "c")
+                        .filter(s -> s.startsWith("a"));
+
+        stream.anyMatch(s -> true);    // ok
+        stream.noneMatch(s -> true);   // exception
+        */
+        Supplier<Stream<String>> streamSupplier =
+                () -> Stream.of("d2", "a2", "b1", "b3", "c")
+                        .filter(s -> s.startsWith("a"));
+
+        streamSupplier.get().anyMatch(s -> true);   // ok
+        streamSupplier.get().noneMatch(s -> true);  // ok
 
     }
 }
